067c0e0a055dc1cdf0f95a3662cd29878ca3af47
Anna Koshyna <ann.koshina@gmail.com>
2017-09-02 23:09:47 +0300

    texture loaded

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ var scene = (function () {
 
     var scene = new THREE.Scene(),
         renderer = new THREE.WebGLRenderer({alpha: true}),
-        camera, earth;
+        camera, earth, material;
 
     function initScene() {
         renderer.setSize(window.innerWidth, window.innerHeight);
@@ -20,17 +20,33 @@ var scene = (function () {
         camera.position.set(-25, 0, 230);
         scene.add(camera);
 
-        var material = new THREE.MeshBasicMaterial({wireframe: true, color: 0xA1D490});
+        //var material = new THREE.MeshBasicMaterial({wireframe: true, color: 0xA1D490});
 
-        earth = new THREE.Mesh(new THREE.SphereGeometry(50, 50, 50), material);
-        earth.position.x = 0;
-        scene.add(earth);
+        // instantiate a loader
+        var loader = new THREE.TextureLoader();
 
-        render();
+        // load a resource
+        loader.load('textures/earth.jpg',
+            function ( texture ) {
+                material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5  } );
+                earth = new THREE.Mesh(new THREE.SphereGeometry(50, 50, 50), material);
+                earth.position.x = 0;
+                scene.add(earth);
+
+                render();
+            },
+	        // Function called when download progresses
+	        function ( xhr ) {
+		        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	        },
+	        // Function called when download errors
+	        function ( xhr ) {
+		        console.log( 'An error happened' );
+        });
     }
 
     function render() {
-        earth.rotation.y += 0.02;
+        earth.rotation.y += 0.005;
         renderer.render(scene, camera);
         requestAnimationFrame(render);
     }
